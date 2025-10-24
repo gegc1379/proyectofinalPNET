6669a71df9cc1b354992efedfb5cb959ac946dc0
Guillermo Gonzalez <guillegonzalez9402@gmail.com>
2025-10-24 16:22:18 -0300

    Agregar archivos de proyecto.

---
+++ FormLogin.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCorreo = New System.Windows.Forms.TextBox()
+        Me.txtContraseña = New System.Windows.Forms.TextBox()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(199, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Correo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(190, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Contraseña"
+        '
+        'txtCorreo
+        '
+        Me.txtCorreo.Location = New System.Drawing.Point(171, 77)
+        Me.txtCorreo.Name = "txtCorreo"
+        Me.txtCorreo.Size = New System.Drawing.Size(100, 20)
+        Me.txtCorreo.TabIndex = 2
+        '
+        'txtContraseña
+        '
+        Me.txtContraseña.Location = New System.Drawing.Point(171, 137)
+        Me.txtContraseña.Name = "txtContraseña"
+        Me.txtContraseña.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtContraseña.Size = New System.Drawing.Size(100, 20)
+        Me.txtContraseña.TabIndex = 3
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Location = New System.Drawing.Point(171, 178)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(100, 23)
+        Me.btnIngresar.TabIndex = 4
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'FormLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 298)
+        Me.Controls.Add(Me.btnIngresar)
+        Me.Controls.Add(Me.txtContraseña)
+        Me.Controls.Add(Me.txtCorreo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "FormLogin"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtCorreo As TextBox
+    Friend WithEvents txtContraseña As TextBox
+    Friend WithEvents btnIngresar As Button
+End Class
---
+++ FormLogin.vb
@@ -0,0 +1,40 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class FormLogin
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        Dim correo As String = txtCorreo.Text.Trim()
+        Dim contraseña As String = txtContraseña.Text.Trim()
+
+        If String.IsNullOrEmpty(correo) OrElse String.IsNullOrEmpty(contraseña) Then
+            MessageBox.Show("Por favor, ingrese correo y contraseña.", "Campos Vacíos", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Dim conn As MySqlConnection = ModuloConexion.GetConexion()
+        If conn Is Nothing Then Return
+
+        Try
+            Dim query As String = "SELECT COUNT(*) FROM usuarios WHERE Correo = @correo AND Contraseña = @pass"
+
+            Using cmd As New MySqlCommand(query, conn)
+
+                cmd.Parameters.AddWithValue("@correo", correo)
+                cmd.Parameters.AddWithValue("@pass", contraseña)
+
+                Dim resultado As Integer = Convert.ToInt32(cmd.ExecuteScalar())
+
+                If resultado > 0 Then
+                    MessageBox.Show("¡Bienvenido!", "Login Exitoso", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+                Else
+                    MessageBox.Show("Correo o contraseña incorrectos.", "Error de Login", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show("Error al intentar iniciar sesión: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            ModuloConexion.Desconectar()
+        End Try
+    End Sub
+End Class
